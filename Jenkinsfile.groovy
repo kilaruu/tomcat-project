f615fc0c6c7868b5b4d82b38698a8d1965be4064
Yogivishnu95 <152314938+Yogivishnu95@users.noreply.github.com>
2023-12-26 09:54:30 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,36 +1,19 @@
-pipeline {
-    agent any
+node {
+    
+    stage('git clone') {
+        git branch: 'main', credentialsId: 'github', url: 'https://github.com/Yogivishnu95/tomcat-project.git'
+  }
+    
 
-    stages {
-        stage('Checkout') {
-            steps {
-                git branch: 'main', url: 'https://github.com/Yogivishnu95/tomcat-project.git'                checkout scm
-            }
-        }
-        stage('Build') {
-            stage('Build') {
-            steps {
-				 sh 'mvn clean install'
-			}
-			post{
-				success{
-					echo "Archiving the Artifacts"
-					archiveArtifacts artifacts: '**/target/*.war'
-				}
-			}
-        }
-        }
-        stage('Test') {
-            steps {
-                // Run tests
-                sh 'mvn test'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                deploy adapters: [tomcat9(credentialsId: 'dc6546fc-d520-4402-8c40-9160a8549f30', path: '', url: 'http://3.111.171.118:8080/')], contextPath: null, war: '**/*.war'
-                sh 'deploy.sh'
-            }
-        }
+    stage('Build') {
+        sh 'mvn clean package'
+    }
+
+    stage('Push') {
+        echo 'This is Push Stage'
+    }
+
+    stage('Deploy') {
+        sh 'sudo cp /home/ec2-user/git/tomcat-project/target/TomcatMavenApp-2.0.war /home/ec2-user/apache-tomcat-9.0.83/webapps'
     }
 }
