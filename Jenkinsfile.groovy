11ee341d8a2d010cd2bb41c58889fae259990c77
Yogivishnu95 <152314938+Yogivishnu95@users.noreply.github.com>
2023-12-26 10:38:23 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,17 @@
 node {
     
-    stage('git clone') {
+    stage('Checkout') {
         git branch: 'main', credentialsId: 'github', url: 'https://github.com/Yogivishnu95/tomcat-project.git'
   }
     
-
+    stage('Test') {
+        echo 'This is Push Stage'
+    }
+    
     stage('Build') {
         sh 'mvn clean package'
     }
-
-    stage('Push') {
-        echo 'This is Push Stage'
-    }
-
+ 
     stage('Deploy') {
         sh 'sudo cp /home/ec2-user/git/tomcat-project/target/TomcatMavenApp-2.0.war /home/ec2-user/apache-tomcat-9.0.83/webapps'
     }
